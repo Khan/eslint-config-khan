6a9fb19c786f5450ac413ec2beca56decb60f24f
Kevin Barabash <kevinbarabash@khanacademy.org>
2019-12-06 18:07:41 -0500

    Use constants instead of 0, 1, 2 (#3)
    * Set up pretty-quick and pre-commit hook
    
    * Cleanup files to make sure things are working as expected
    
    * Use constants instead of 0, 1, 2
    
    * actually commit .prettierrc.js
    
    * get the path right
    
    * Use constants for everything in flow.js

--- flow.js
+++ flow.js
@@ -1,33 +1,38 @@
 // Flow static typing lint rules
 // https://github.com/gajus/eslint-plugin-flowtype
 // https://flowtype.org/
+
+const OFF = "off";
+const WARN = "warn";
+const ERROR = "error";
+
 module.exports = {
     rules: {
-        "flowtype/boolean-style": [2, "boolean"],
-        "flowtype/define-flow-type": 1, // suppress no-undef on flow types
-        "flowtype/no-dupe-keys": 2,
+        "flowtype/boolean-style": [ERROR, "boolean"],
+        "flowtype/define-flow-type": WARN, // suppress no-undef on flow types
+        "flowtype/no-dupe-keys": ERROR,
 
         // Use Flow's version of no-unused-expressions
         "flowtype/no-unused-expressions": [
-            2,
+            ERROR,
             {allowShortCircuit: true, allowTernary: true},
         ],
-        "no-unused-expressions": 0, // Disable this rule as flowtype/no-unused-expression supersedes it
+        "no-unused-expressions": OFF, // Disable this rule as flowtype/no-unused-expression supersedes it
 
-        "flowtype/no-weak-types": 0, // allow 'any' for now
+        "flowtype/no-weak-types": OFF, // allow 'any' for now
         // flow may still require parameter types in certain situations
-        "flowtype/require-parameter-type": 0,
-        "flowtype/require-return-type": 0,
+        "flowtype/require-parameter-type": OFF,
+        "flowtype/require-return-type": OFF,
         "flowtype/require-valid-file-annotation": [
-            2,
+            ERROR,
             "always",
             {
                 annotationStyle: "line",
             },
         ],
-        "flowtype/sort": 0,
-        "flowtype/type-id-match": 0,
-        "flowtype/use-flow-type": 1, // suppress no-unused-vars on flow types
+        "flowtype/sort": "",
+        "flowtype/type-id-match": OFF,
+        "flowtype/use-flow-type": WARN, // suppress no-unused-vars on flow types
     },
     settings: {
         flowtype: {
--- shared.js
+++ shared.js
@@ -7,59 +7,64 @@
 //
 // This rc file is not meant to be used directly; rather it is extended
 // by the user-facing files index.js, browser.js, and node.js.
+
+const OFF = "off";
+const WARN = "warn";
+const ERROR = "error";
+
 module.exports = {
     rules: {
         // We'd possibly like to remove the 'properties': 'never' one day.
         camelcase: [
-            2,
+            ERROR,
             {
                 properties: "never",
                 allow: ["^UNSAFE_"],
             },
         ],
-        curly: 2,
-        eqeqeq: [2, "allow-null"],
-        "guard-for-in": 2,
-        "linebreak-style": [2, "unix"],
-        "max-lines": [2, 1000],
-        "no-alert": 2,
-        "no-array-constructor": 2,
-        "no-console": 2,
-        "no-debugger": 2,
-        "no-dupe-class-members": 2,
-        "no-dupe-keys": 2,
-        "no-extra-bind": 2,
-        "no-new": 2,
-        "no-new-func": 2,
-        "no-new-object": 2,
-        "no-throw-literal": 2,
-        "no-undef": 2,
-        "no-unexpected-multiline": 2,
-        "no-unreachable": 2,
+        curly: ERROR,
+        eqeqeq: [ERROR, "allow-null"],
+        "guard-for-in": ERROR,
+        "linebreak-style": [ERROR, "unix"],
+        "max-lines": [ERROR, 1000],
+        "no-alert": ERROR,
+        "no-array-constructor": ERROR,
+        "no-console": ERROR,
+        "no-debugger": ERROR,
+        "no-dupe-class-members": ERROR,
+        "no-dupe-keys": ERROR,
+        "no-extra-bind": ERROR,
+        "no-new": ERROR,
+        "no-new-func": ERROR,
+        "no-new-object": ERROR,
+        "no-throw-literal": ERROR,
+        "no-undef": ERROR,
+        "no-unexpected-multiline": ERROR,
+        "no-unreachable": ERROR,
         // NOTE: If you change the options here, be sure to update flow.js also
         "no-unused-expressions": [
-            2,
+            ERROR,
             {allowShortCircuit: true, allowTernary: true},
         ],
-        "no-unused-vars": [2, {args: "none", varsIgnorePattern: "^_*$"}],
-        "no-useless-call": 2,
-        "no-with": 2,
-        "one-var": [2, "never"],
-        "jsx-a11y/alt-text": 2,
-        "jsx-a11y/aria-props": 2,
-        "jsx-a11y/aria-role": [2, {ignoreNonDOM: true}],
-        "jsx-a11y/anchor-is-valid": 2,
+        "no-unused-vars": [ERROR, {args: "none", varsIgnorePattern: "^_*$"}],
+        "no-useless-call": ERROR,
+        "no-with": ERROR,
+        "one-var": [ERROR, "never"],
+        "jsx-a11y/alt-text": ERROR,
+        "jsx-a11y/aria-props": ERROR,
+        "jsx-a11y/aria-role": [ERROR, {ignoreNonDOM: true}],
+        "jsx-a11y/anchor-is-valid": ERROR,
         // TODO(scottgrant): Add additional a11y rules as we support them.
         // ---------------------------------------
         // Stuff that's disabled for now, but maybe shouldn't be.
         // TODO(jeresig): It's an anti-pattern but it appears to be used
         // frequently in reducers, the alternative would be super-clunky.
-        "no-case-declarations": 0,
+        "no-case-declarations": OFF,
         // TODO(csilvers): enable these if/when community agrees on it.
         // Might be nice to turn this on one day, but since we don't
         // use jsdoc anywhere it seems silly to require it yet.
-        "valid-jsdoc": 0,
-        "require-jsdoc": 0,
+        "valid-jsdoc": OFF,
+        "require-jsdoc": OFF,
     },
     globals: {
         // These are copied from
